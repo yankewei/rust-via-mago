862e3144b76d2a6c7bd2591bb8f82a8edb0d2a70
yankewei <yankewei1993@gmail.com>
2025-07-22 21:43:21 +0800

    Rust study one

--- rust-study-one/src/main.rs
+++ rust-study-one/src/main.rs
@@ -1,7 +1,9 @@
 use mago_php_version::PHPVersion;
+use std::str::FromStr;
 
 fn main() {
-    let version = PHPVersion::new(8, 4, 0);
+    // let version = PHPVersion::new(8, 4, 0);
+    let version = PHPVersion::from_str("8.4.0").unwrap();
     println!("Major version: {}", version.major());
     println!("Minor version: {}", version.minor());
     println!("Patch version: {}", version.patch());
